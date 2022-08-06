a8b7840dab022f617a9d03e8b1c72673350f0e11
Amanda Perry <challenge641@gmail.com>
2022-08-06 16:41:18 -0400

    filterbyQuery()

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const { animals } = require('./data/animals');
 // instantiate the server
 const app = express();
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
 
 // to ADD the route referenced above:
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+        res.json(results);
 });
 
 // chain the listen() method onto our new server
