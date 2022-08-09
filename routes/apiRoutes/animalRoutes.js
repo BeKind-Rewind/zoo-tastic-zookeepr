ed8003eb08518892ca504d55d50be35647f2bb18
Amanda Perry <challenge641@gmail.com>
2022-08-09 18:39:08 -0500

    added functionality to zookeepers and the dataset plus fixed some css issue

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -4,28 +4,28 @@ const { animals } = require('../../data/animals');
 
 // to ADD the route referenced above:
 router.get('/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-        res.json(results);
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
 
 router.get('/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-      } else {
-        res.send(404);
-      }
-  });
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 // req.query is multifaceted, often combining multiple parameters, whereas req.
 // param is specific to a single property, often intended to retrieve a single record.
 
 
 
-router.post('/api/animals', (req, res) => {
+router.post('/animals', (req, res) => {
     // req.body is where our incoming content will be
     // generate the id value on the server (the server can see all of the data so there are no duplicates)
     // in this case, we are telling to generate +1 to the highest id since the array is marked in order
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-// create a route that the front-end can request data from
-const { animals } = require('./data/animals');
 
 // setting envar for port in case one is set; if not then use 3001
 const PORT = process.env.PORT || 3001;
-
 // instantiate the server
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -18,9 +15,10 @@ app.use(express.json());
 // can now be accessed (in public folder) without having a specific server endpoint created for it
 app.use(express.static('public'));
 
-
-
-
+// Tell the server any time a client navigates to <ourhost>/api, the app will use the router we set up in apiRoutes. 
+// If '/' is the endpoint, then the router will serve back our HTML routes.
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // chain the listen() method onto our new server
 app.listen(PORT, () => {
